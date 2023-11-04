2f0c736b7af1b9774d4bc7509f5f9a278b7ce4a9
Ibrahim <114424880+ibrahim-miloua@users.noreply.github.com>
2023-11-04 19:32:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function imageFunc(pos){
+    var randomNumber = Math.floor(Math.random() * 6 )+1;// 1-6
+
+    var diceName=  "dice"+randomNumber+".png"// dice1.png -6
+    
+    var image = "images/"+diceName; //full name
+    
+    var image1 = document.querySelectorAll("img")[pos];
+    
+    image1.setAttribute("src",image);
+
+    return randomNumber;
+}
+
+num1 = imageFunc(0);
+num2 = imageFunc(1);
+
+if(num1>num2){
+document.querySelector("h1").innerHTML = "PLAYER 1 WINS";
+}
+else if(num1<num2){
+    document.querySelector("h1").innerHTML = "PLAYER 2 WINS";
+
+}
+else
+document.querySelector("h1").innerHTML = "DRAW";
